d329de527c5fa5b1787c44c9c4ed29279b44f10f
Yui Otaka <yuiotaka145@gmail.com>
2021-01-24 19:26:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Nabvar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home.js';
+import aboutme from './components/pages/Aboutme.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+       <Nabvar />
+       <Switch>
+       <Route path='/' exact component={Home} />
+       <Route path='/aboutme' component={aboutme} />
+       </Switch>
+       </Router>
+      </>
   );
 }
 
+//ビデオは５３；３８までみた。https://www.youtube.com/watch?v=I2UBjN5ER4s&t=1359s
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../App.css';
+import HeroSectoin from '../HeroSection';
+import Cards from '../Card.js';
+import Footer from '../Footer.js';
+
+
+
+function Home() {
+    return (
+        <>
+        
+            <HeroSectoin />
+            <Cards />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
